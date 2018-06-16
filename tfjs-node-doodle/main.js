25b46c5071e0c3f0d3509fb7be86e01e92eb2f2b
Shanqing Cai <cais@google.com>
2018-06-15 23:11:35 -0400

    Simplify main.js further

--- tfjs-node-doodle/main.js
+++ tfjs-node-doodle/main.js
@@ -1,22 +1,19 @@
 const tf = require('@tensorflow/tfjs');
-
 require('@tensorflow/tfjs-node');
 
-console.log(tf.version);
+(async function() {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({units: 10, inputShape: [5], activation: 'relu'}));
+  model.add(tf.layers.dense({units: 4, activation: 'softmax'}));
 
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 10, inputShape: [5], activation: 'relu'}));
-model.add(tf.layers.dense({units: 4, activation: 'softmax'}));
+  const xs = tf.ones([2, 5]);
+  model.predict(xs).print();
 
-const xs = tf.zeros([2, 5]);
-model.predict(xs).print();
+  const saveLocation = 'file:///tmp/tfjs-node-model-1';
 
-const saveLocation = 'file:///tmp/tfjs-node-model-1';
-
-model.save(saveLocation).then(saveResult => {
+  const saveResult = await model.save(saveLocation);
   console.log(saveResult);
 
-  tf.loadModel(saveLocation + '/model.json').then(model2 => {
-    model2.predict(xs).print();
-  });
-});
+  const model2 = await tf.loadModel(saveLocation + '/model.json');
+  model2.predict(xs).print();
+})();
