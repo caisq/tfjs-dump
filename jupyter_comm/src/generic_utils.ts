4d244d7b4855b4f6095e4411d1f863b8898c3039
Shanqing Cai <cais@google.com>
2019-06-10 14:00:46 -0700

    Remove jupyter_comm

--- jupyter_comm/src/generic_utils.ts
+++
@@ -1,8 +0,0 @@
-export async function sleep(timeMs: number): Promise<void>{
-  console.log(`In sleep(): ${timeMs} ms`);  // DEBUG
-  return new Promise((resolve) => {
-    setTimeout(() => {
-    resolve();
-    }, timeMs);
-  });
-}
